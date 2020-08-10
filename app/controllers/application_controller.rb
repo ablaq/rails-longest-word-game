e185ffd0ce5f92a659d22e2f34c3338362c4b4ac
Abdel Laqdim <al@kidsandplay.be>
2020-08-10 17:09:24 +0200

    word game

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  def new
+   @grid = Array.new(8){("A".."Z").to_a.sample}
+  end
+
+  def score
+    if params[:grid]
+      result = params[:grid]
+
+       result.split.each do |letter|
+        if
+          @grid.include?(letter)
+          @display = "good job"
+        else
+          @display = "fuck"
+        end
+      end
+    end
+  end
 end
